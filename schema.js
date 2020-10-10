1f13865ae7737981f7b509eb25956110f84ed701
wjonke <wesjonke@gmail.com>
2020-10-10 12:48:19 -0400

    added hard coded "customers list"

--- schema.js
+++ schema.js
@@ -7,6 +7,14 @@ const {
   GraphQLNonNull,
 } = require("graphql");
 
+//Hard coded data just for now, JSON Server to be used later
+const customers = {
+  id: "1",
+  name: "Wes Jonke",
+  email: "test@gmail.com",
+  age: 37,
+};
+
 //customer type
 //used in the root, types and structure set up here
 const CustomerType = new GraphQLObjectType({
