d770937b226b5f9dc9c01373dafeafc293945032
wjonke <wesjonke@gmail.com>
2020-10-10 12:50:08 -0400

    added a few more "customers'

--- schema.js
+++ schema.js
@@ -8,12 +8,12 @@ const {
 } = require("graphql");
 
 //Hard coded data just for now, JSON Server to be used later
-const customers = {
-  id: "1",
-  name: "Wes Jonke",
-  email: "test@gmail.com",
-  age: 37,
-};
+const customers = [
+  { id: "1", name: "Wes Jonke", email: "test@gmail.com", age: 37 },
+  { id: "2", name: "Sam Smith", email: "test@gmail.com", age: 22 },
+  { id: "3", name: "Jim Jones", email: "test@gmail.com", age: 34 },
+  { id: "4", name: "somyoung guuy", email: "test@gmail.com", age: 89 },
+];
 
 //customer type
 //used in the root, types and structure set up here
